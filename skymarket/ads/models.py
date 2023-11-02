3f94b26721ad1f72887081ed0111207c536677db
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-11-02 16:47:01 +0300

    Ad and Comment model

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,40 @@
-from django.conf import settings
+from datetime import datetime
+
 from django.db import models
+
+from users.models import User
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    """Модель Объявления"""
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000, blank=True)
+    price = models.PositiveIntegerField()
+    author = models.ForeignKey(User, related_name="ads", on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    """Модель Комментария"""
+
+    text = models.TextField(max_length=1000)
+    author = models.ForeignKey(User, related_name="comments", on_delete=models.SET_NULL, null=True)
+    ad = models.ForeignKey(Ad, related_name="comments", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        text = str(self.text)
+        return text if len(text) <= 20 else text[:20] + "..."
