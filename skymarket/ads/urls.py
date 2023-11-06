7d1b781cd37b030f860bbe77b0d8dfa0f5a1ecf4
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-11-07 00:36:55 +0300

    urls.py for Ads

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,43 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from rest_framework_nested import routers
+from rest_framework_nested.routers import NestedSimpleRouter
 
-from ads.apps import SalesConfig
-from ads.views import AdViewSet, CommentViewSet, UserAdsListView
-
-app_name = SalesConfig.name
-
-# ads_router = SimpleRouter()
-# ads_router.register('', AdViewSet)
-# comments_router = SimpleRouter()
-# comments_router.register('', CommentViewSet)
-#
-# urlpatterns = [
-#     path('me/', UserAdsListView.as_view(), name='user_ads'),
-#     path('', include(ads_router.urls)),
-#     path('<int:ad_pk>/comments/', include(comments_router.urls), name='comments'),
-# ]
-
-
-#
-# ads_router = routers.SimpleRouter()
-# ads_router.register('', AdViewSet, basename='ad')
-# comments_router = routers.NestedSimpleRouter(ads_router, '', lookup='ad')
-# comments_router.register(r'(?P<ad_id>\d+)/comments', CommentViewSet, basename='ad_comments')
-#
-# urlpatterns = [
-#     path('', include(ads_router.urls)),
-#     path('', include(comments_router.urls)),
-# ]
-
+from ads.views import AdViewSet, CommentViewSet
 
 ads_router = SimpleRouter()
-ads_router.register('', AdViewSet)
-comments_router = SimpleRouter()
-comments_router.register('', CommentViewSet)
+ads_router.register('ads', AdViewSet, basename='ads')
+
+comments_router = NestedSimpleRouter(ads_router, 'ads', lookup='ad')
+comments_router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('me/', UserAdsListView.as_view(), name='user_ads'),
     path('', include(ads_router.urls)),
-    path('<int:ad_pk>/comments/', include(comments_router.urls), name='comments'),
+    path('', include(comments_router.urls)),
 ]
