192aec84fb76f5f17fe49c5b338dc712839dfb6e
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-11-09 15:05:48 +0300

    minor changes

--- skymarket/users/views.py
+++ skymarket/users/views.py
@@ -1,13 +1,6 @@
-from rest_framework import permissions
-from rest_framework.decorators import (
-    api_view,
-    permission_classes,
-)
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-import requests
-
 
 import requests
 
